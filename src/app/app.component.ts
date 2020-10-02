e19ecee92686154788eef79d1426d721a0f71f72
quangloc.nguyen <you@example.com>
2020-10-02 15:01:01 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'youtube-ang';
+  name = 'burce';
+
+  getName() {
+    return this.name;
+  }
+
+  obj = {
+    name: 'kelly',
+    age: 20,
+  };
+
+  arr=['david','ddnn2026','american']
 }
